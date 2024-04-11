96af058de535973498e54fd7b9c372ec2390d7d0
Akchaya-A <147383376+Akchaya-A@users.noreply.github.com>
2024-04-11 11:45:40 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('not_found/', views.not_found, name='not_found'),
+    path('teacherhome/', views.teacherHome, name='teacherhome'),
+    path('upload/', views.upload_form, name='upload_form'),
+    path('download/<int:file_id>/', views.download_file, name='download_file'),
+    path('delete/<int:file_id>/', views.delete_file, name='delete_file'),
 ]
